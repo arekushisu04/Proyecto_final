42c139b9eea043587af51f1f2930fcf8ca34e444
arekushisu04 <108507090+arekushisu04@users.noreply.github.com>
2023-01-14 20:11:27 -0600

    Update ProyectoFinal.cpp

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -4,6 +4,9 @@ int main()
 {
     printf("Hello world");
     
+    //Declaración de variables para 
     char string1[] = "nickname:password:name:fatherLastName:motherLastName:email:number";
     char string2[] = ":";
+    char personalDate = *
+    char personalDate = *
 }
